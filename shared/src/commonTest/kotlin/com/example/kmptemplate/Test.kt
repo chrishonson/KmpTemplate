418902b1f2211a1b8f0718386d8cfd6353e704a5
Nicholas Christensen <chrishonson@gmail.com>
2023-11-25 18:09:16 -0600

    unit tests

---
+++ shared/src/commonTest/kotlin/com/example/kmptemplate/Test.kt
@@ -0,0 +1,39 @@
+package com.example.kmptemplate
+
+import Greeting
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.test.runTest
+import kotlinx.serialization.json.Json
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class CommonGreetingTest {
+
+    fun testExample() {
+        runTest{
+            val mockEngine = MockEngine { request ->
+                respond(
+                    content = ByteReadChannel("""[{"name":"CTS21","flight_number":42,"success":true,"date_utc":"2021-09-15T00:00:00Z"}]"""),
+                    status = HttpStatusCode.OK,
+                    headers = headersOf(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                )
+            }
+
+            val mockHttpClient = HttpClient(mockEngine) {
+                install(ContentNegotiation) {
+                    json(Json {
+                        prettyPrint = true
+                        isLenient = true
+                        ignoreUnknownKeys = true
+                    })
+                }
+            }
+            assertTrue("Check 2021-09 is mentioned", Greeting(mockHttpClient).greet().contains("Greetings!"))
+
+        }
+    }
+}
